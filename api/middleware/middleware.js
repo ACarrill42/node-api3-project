aef585be922f81c7dc16dcad94bdbbfc849e739c
ACarrill42 <austincarrill@gmail.com>
2021-09-08 10:57:45 -0500

    middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,34 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+
+  next();
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  const {id} = req.params;
+
+  if(id === req.user) {
+    res.status(200).json(req.user)
+  }else {
+    res.status(404).json({message: 'user not found'})
+  }
+
+  next();
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+
+  next();
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+
+  next();
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {logger, validateUser, validateUserId, validatePost};
